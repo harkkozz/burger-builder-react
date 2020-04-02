6c2fe238d514f7b4aea54e6ec2bcf40e4eda713d
hadilovic <adilovicharis94@gmail.com>
2020-04-02 16:55:15 +0200

    WIP

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Wrapper from '../../hoc/Wrapper';
+import classes from './Layout.module.css';
+
+const layout = props => (
+  <Wrapper>
+     <div>
+      Toolbar, Sidebar, Backdrop
+    </div>
+    <main className={ classes.Content }>
+      { props.children }
+    </main>
+  </Wrapper>
+)
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Wrapper from '../../high-order-components/Wrapper';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    }
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Burger ingredients={ this.state.ingredients }/>
+        <div>Build Controls (ingredients)</div>
+      </Wrapper>
+    );
+  }
+}
+
+export default BurgerBuilder;
